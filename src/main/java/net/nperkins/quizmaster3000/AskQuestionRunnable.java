00bfb2237c2a6a452a9fe207f6ce36eed9494718
Nick Perkins <nick@ohbees.com.au>
2014-07-07 21:00:56 +1000

    If no answer provided, ask a question...

--- src/main/java/net/nperkins/quizmaster3000/AskQuestionRunnable.java
+++ src/main/java/net/nperkins/quizmaster3000/AskQuestionRunnable.java
@@ -51,11 +51,10 @@ class AskQuestionRunnable implements Runnable {
             plugin.sendPlayers(MessageFormat.format(plugin.getMessages().getString("quiz.question.answer"), StringUtils.join(Arrays.copyOfRange(plugin.getCurrentQuestion().getAnswer(), 0, plugin.getCurrentQuestion().getAnswer().length), plugin.getMessages().getString("quiz.answer.joiner"))));
             if (plugin.getScores().size() == 0) {
                 plugin.sendPlayers(plugin.prefixMessage(plugin.getMessages().getString("error.allplayersleft")));
-                Bukkit.getScheduler().cancelTask(id);
                 plugin.setState(QuizState.FINISHED);
                 plugin.setRunning(false);
+            } else{
                 plugin.setState(QuizState.WAITFORNEXT);
-                Bukkit.getScheduler().cancelTask(id);
                 plugin.sendPlayers(plugin.getMessages().getString("quiz.question.next"));
                 plugin.getWaitForNextRunnable().start();
             }
